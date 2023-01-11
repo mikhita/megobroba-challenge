2bf1d5709f20438e1654d256997282b3347a2a1d
mikhita <mikheilispiriani@yahoo.com>
2023-01-12 02:40:48 +0400

    I complete working on mobile screen version and now project functionality is ready to add large screen styles

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+let leftPrice = document.getElementById("left");
+let rightPrice = document.getElementById("right");
+let slider = document.getElementById("sliderDiv");
+let span = document.querySelector("dot");
+let elements = document.querySelectorAll(".arrowOpenClose");
+
+for (let i = 0; i < elements.length; i++) {
+  elements[i].onclick = function () {
+    let answer = this.parentElement.nextSibling.nextSibling;
+    console.log(answer.style.display);
+    if (answer.style.display === "" || answer.style.display === "none") {
+      this.style.transform = "rotate(180deg)";
+      answer.style.display = "block";
+    } else {
+      console.log(this);
+      this.style.transform = "rotate(0)";
+      answer.style.display = "none";
+    }
+  };
+}
+
 function changeLeft() {
-  let leftPrice = document.getElementById("left");
-  let rightPrice = document.getElementById("right");
   leftPrice.classList.add("backGreen");
   rightPrice.classList.remove("backGreen");
 }
 function changeRight() {
-  let leftPrice = document.getElementById("left");
-  let rightPrice = document.getElementById("right");
   leftPrice.classList.remove("backGreen");
   rightPrice.classList.add("backGreen");
+}
+
+function slideItem(n) {
+  if (n === 1) {
+    slider.style.transform = "translate(0px, 0px)";
+  } else if (n === 2) {
+    slider.style.transform = "translate(-28%, 0%)";
+  } else if (n === 3) {
+    slider.style.transform = "translate(-60%, 0%)";
+  }
 }
