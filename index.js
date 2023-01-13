13b8c45cee3801c1340487372df33fd1d7326f7d
mikhita <mikheilispiriani@yahoo.com>
2023-01-13 19:47:30 +0400

    I almost complete this challenge left only to turn arrows while click on faaq

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 let slider = document.getElementById("sliderDiv");
-let span = document.querySelector("dot");
+let span = document.querySelectorAll(".dot");
 let elements = document.querySelectorAll(".arrowOpenClose");
+let questions = document.querySelectorAll(".question");
+let hrAndAnswers = document.querySelectorAll(".hrAndAnswer");
+let faq = document.querySelector(".faq");
 
 for (let i = 0; i < elements.length; i++) {
   elements[i].onclick = function () {
     let answer = this.parentElement.nextSibling.nextSibling;
-    console.log(answer.style.display);
     if (answer.style.display === "" || answer.style.display === "none") {
+      e.stopPropagation();
       this.style.transform = "rotate(180deg)";
       answer.style.display = "block";
     } else {
-      console.log(this);
+      e.stopPropagation();
       this.style.transform = "rotate(0)";
       answer.style.display = "none";
     }
   };
 }
+for (let i = 0; i < questions.length; i++) {
+  questions[i].onclick = function () {
+    let answer = this.parentElement.childNodes[3];
+    let arrow = this.parentElement.childNodes[1].childNodes[3];
+    if (answer.style.display === "" || answer.style.display === "none") {
+      arrow.style.transform = "rotate(180deg)";
+      answer.style.display = "block";
+      answer.style.visibility = "visible";
+      answer.style.opacity = "1";
+    } else {
+      arrow.style.transform = "rotate(0deg)";
+      answer.style.display = "none";
+    }
+  };
+}
+for (let i = 0; i < hrAndAnswers.length; i++) {
+  hrAndAnswers[i].onclick = function () {
+    this.style.display = "none";
+    this.parentElement.childNodes[1].childNodes[3].style.transform =
+      "rotate(0deg)";
+  };
+}
 
+for (let i = 0; i < span.length; i++) {
+  span[i].onclick = function () {
+    for (let j = 0; j < span.length; j++) {
+      span[j].style.background = "#CBCED6";
+    }
+    this.style.background = "blue";
+  };
+}
+
+faq.addEventListener("click", function () {
+  if (
+    faq.childNodes[3].style.transform === "" ||
+    faq.childNodes[3].style.transform === "rotate(180deg)"
+  ) {
+    for (let i = 0; i < hrAndAnswers.length; i++) {
+      hrAndAnswers[i].style.display = "block";
+    }
+    faq.childNodes[3].style.transform = "rotate(0deg)";
+  } else {
+    for (let i = 0; i < hrAndAnswers.length; i++) {
+      hrAndAnswers[i].style.display = "none";
+    }
+    faq.childNodes[3].style.transform = "rotate(180deg)";
+  }
+});
 function slideItem(n) {
   if (n === 1) {
     slider.style.transform = "translate(0px, 0px)";
